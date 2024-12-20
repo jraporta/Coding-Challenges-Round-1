e7d513224804f9575bf106f95c42214636390a56
jraporta <jraporta@gmail.com>
2024-12-20 14:38:48 +0100

    add email already exists verification

---
+++ src/main/java/com/hackathon/finservice/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.hackathon.finservice.Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
--- src/main/java/com/hackathon/finservice/Service/UserService.java
+++ src/main/java/com/hackathon/finservice/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.hackathon.finservice.Service;
 
 import com.hackathon.finservice.Entities.User;
+import com.hackathon.finservice.Exception.EmailAlreadyExistsException;
 import com.hackathon.finservice.Repositories.UserRepository;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ public class UserService {
     UserRepository userRepository;
 
     public User createUser(@NotEmpty String name, @NotEmpty String email, @NotEmpty String hashedPassword) {
+        if (userRepository.existsByEmail(email)) throw new EmailAlreadyExistsException("Email already exists");
         return userRepository.save(new User(name, email, hashedPassword));
     }
 }
