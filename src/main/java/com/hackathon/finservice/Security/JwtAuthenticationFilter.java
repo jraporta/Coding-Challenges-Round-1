ef6b3f4682bab10f59a48613f0f071389b0cdee3
jraporta <jraporta@gmail.com>
2024-12-23 01:17:02 +0100

    User Details endpoint error message

--- src/main/java/com/hackathon/finservice/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/hackathon/finservice/Security/JwtAuthenticationFilter.java
@@ -43,7 +43,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         if (isProtectedEndpoint(request)) {
             log.debug("Validating JWT");
-            executeFilter(request, response);
+            if (!executeFilter(request, response)) {
+                return;
+            }
         }
         filterChain.doFilter(request, response);
     }
@@ -56,7 +58,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 path.contains("health"));
     }
 
-    protected void executeFilter(HttpServletRequest request, HttpServletResponse response)
+    protected boolean executeFilter(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         final String authorizationHeader = request.getHeader(header);
@@ -64,6 +66,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (authorizationHeader == null) {
             log.debug("Error, missing authorization header");
             sendErrorResponse(response, "Access Denied");
+            return false;
         }
 
         String jwtToken = null;
@@ -77,9 +80,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             username = jwtTokenUtil.extractUsername(jwtToken);
             log.debug("Extracted username from token: {}", username);
         }catch (ExpiredJwtException ex) {
+            log.debug("Expired Jwt");
             sendErrorResponse(response, "Invalid JWT: token is expired");
+            return false;
         }catch (JwtException ex) {
+            log.debug("Invalid JWT token");
             sendErrorResponse(response, "Invalid JWT token");
+            return false;
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -91,11 +98,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        return true;
     }
 
     private void sendErrorResponse(HttpServletResponse response, String message) throws IOException {
+        log.debug("Creating custom error response: {}", message);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-        response.getWriter().write(message);
+        response.getWriter().write("Access Denied");
     }
 }
